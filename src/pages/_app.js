8faa68b6c66f1f2475ea800b01d8835523f10f87
Ari <arilife.com@gmail.com>
2024-06-21 01:51:08 +0900

    💄 Adding Font family

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,17 @@
 import "tailwindcss/tailwind.css";
-import { AppLayout } from "../layouts";
+import "../styles/global.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AppLayout>
-      <Component {...pageProps} />
-    </AppLayout>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main className="bg-light w-full min-h-screen">
+        <Component {...pageProps} />
+      </main>
+    </>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: "JIT",
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
-      colors: {
-        pink: "#faeee7",
-      },
       fontFamily: {
-        arial: ["Arial"],
+        zen: ['"Zen Maru Gothic"', "sans-serif"],
       },
     },
   },
