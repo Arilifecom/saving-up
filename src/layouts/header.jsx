6870faacc2d9815c1a32fcd9ea516147f91f8fcd
Ari <arilife.com@gmail.com>
2024-06-21 01:49:59 +0900

    ♻️Deleting unused compornents

--- src/layouts/header.jsx
+++
@@ -1,9 +0,0 @@
-export function Header() {
-  return (
-    <header className="w-screen h-10 mb-3">
-      <div className="flex max-w-6xl mx-auto pt-1">
-        <h1 className=" font-arial text-2xl font-bold">SavingUP</h1>
-      </div>
-    </header>
-  );
-}
