df41abc09d493f761116c2caf2bcf00db365580a
Ari <arilife.com@gmail.com>
2024-06-21 23:54:23 +0900

    💄 Adding theme color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
+    ontFamily: {
+      zen: ['"Zen Maru Gothic"', "sans-serif"],
+    },
+    screens: {
+      md: { min: "768px" },
+    },
+    colors: {
+      pink: "#FFEAE3",
+      blue: "#121481",
+      primary: "#FF8BA7",
+      light: "#ffffff",
+    },
     extend: {
-      fontFamily: {
-        zen: ['"Zen Maru Gothic"', "sans-serif"],
+      backgroundImage: {
+        "wave-shape-xs": "url('/img/bg-wave-shape-xs.svg')",
+        "wave-shape-xl": "url('/img/bg-wave-shape-xl.svg')",
       },
     },
   },
