3aec82a2f2cc24cc92a195bc1484011634558d8c
Ari <arilife.com@gmail.com>
2024-06-24 12:26:43 +0900

    💄 Changing height

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex items-center justify-between px-4 bg-blue text-pink text-xs md:justify-center md:gap-8">
+    <div className="flex h-[60px] items-center justify-between px-4 bg-blue text-pink text-xs md:justify-center md:gap-8">
       <span>{new Date().getFullYear()} &copy; All Rights reserved.</span>
-      <div className="flex items-center lg:py-2">
-        build Width{" "}
-        <span className="text-primary dark:text-primaryDark text-2xl px-1">
-          &#9825;
-        </span>
+      <div className="flex items-center">
+        build Width <span className="text-primary text-2xl px-1">&#9825;</span>
         by&nbsp;
         <Link
           href="/"
