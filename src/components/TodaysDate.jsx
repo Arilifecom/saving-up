f1100d72b15c74e41f92c66af8877ab639b0f03e
Ari <arilife.com@gmail.com>
2024-06-26 18:03:56 +0900

    🐛 Changed get date Compornent

--- src/components/TodaysDate.jsx
+++ src/components/TodaysDate.jsx
@@ -1,11 +1,14 @@
-import React from "react";
-
 const TodaysDate = () => {
-  const today = new Date().toLocaleDateString();
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, "0");
+  const day = String(today.getDate()).padStart(2, "0");
+  const formattedDate = `${year}/${month}/${day}`;
+
   return (
     <div>
       <label>日付</label>
-      <p className="border mr-2 rounded py-2 px-4">{today}</p>
+      <p className="border mr-2 rounded py-2 px-4">{formattedDate}</p>
     </div>
   );
 };
