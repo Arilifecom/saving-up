8545753dbb4fa2788d5c1c84cd0c7c9d8aa5c260
Ari <arilife.com@gmail.com>
2024-07-22 12:17:58 +0900

    💄 Changing Alert UI

--- src/components/CustomAlert.jsx
+++ src/components/CustomAlert.jsx
@@ -3,13 +3,10 @@ import React from "react";
 const CustomAlert = ({ message, onClose }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50">
-      <div className="max-w-md bg-light border-[3px] border-headline text-headline px-4 py-3 rounded shadow-md">
+      <div className="text-lg bg-[#ffffff] text-headline border-r-headline border-solid border-[2px] px-8 py-10 rounded shadow-md">
         <div className="flex justify-between items-center">
           <span>{message}</span>
-          <button
-            onClick={onClose}
-            className="text-red-700 hover:text-red-500 ml-4"
-          >
+          <button onClick={onClose} className="text-4xl ml-4 text-">
             &times;
           </button>
         </div>
