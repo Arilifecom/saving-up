4397efa05e918bb4901a8b396eea65790d6ec1ad
Ari <arilife.com@gmail.com>
2025-02-13 18:37:29 +0900

    ♻️ refactoring

--- src/utils/calculateResults.jsx
+++ src/utils/calculateResults.jsx
@@ -1,12 +1,18 @@
 export const calculateResults = (inputData, exchangeRate) => {
   const { targetJPY, currentValueJPY, currentValueAUD, targetDate } = inputData;
 
-  const savingAmountJPY =
-    parseInt(currentValueJPY) + parseInt(currentValueAUD) * exchangeRate;
-  const savingAmountAUD =
-    parseInt(currentValueAUD) + parseInt(currentValueJPY) / exchangeRate;
-  const remainingJPY = parseInt(targetJPY) - savingAmountJPY;
-  const remainingAUD = parseInt(targetJPY) / exchangeRate - savingAmountAUD;
+  const parseCurrency = (value) => {
+    return Number(value.replace(/,/g, ""));
+  };
+
+  const targetJPYNum = parseCurrency(targetJPY);
+  const currentValueJPYNum = parseCurrency(currentValueJPY);
+  const currentValueAUDNum = parseCurrency(currentValueAUD);
+
+  const savingAmountJPY = currentValueJPYNum + currentValueAUDNum * exchangeRate;
+  const savingAmountAUD = currentValueAUDNum + currentValueJPYNum / exchangeRate;
+  const remainingJPY = targetJPYNum - savingAmountJPY;
+  const remainingAUD = targetJPYNum / exchangeRate - savingAmountAUD;
 
   const targetDateObj = new Date(targetDate);
   const currentDate = new Date();
@@ -18,48 +24,36 @@ export const calculateResults = (inputData, exchangeRate) => {
   const weeklyTargetAUD = remainingAUD / remainingWeeks;
 
   return {
-    savingAmountJPY: isNaN(savingAmountJPY)
-      ? "N/A"
-      : savingAmountJPY.toLocaleString("ja-JP", {
+    savingAmountJPY: savingAmountJPY.toLocaleString("ja-JP", {
           style: "currency",
           currency: "JPY",
           minimumFractionDigits: 0,
         }),
-    savingAmountAUD: isNaN(savingAmountAUD)
-      ? "N/A"
-      : savingAmountAUD.toLocaleString("en-AU", {
+    savingAmountAUD: savingAmountAUD.toLocaleString("en-AU", {
           style: "currency",
           currency: "AUD",
           minimumFractionDigits: 2,
         }),
-    remainingJPY: isNaN(remainingJPY)
-      ? "N/A"
-      : remainingJPY.toLocaleString("ja-JP", {
+    remainingJPY: remainingJPY.toLocaleString("ja-JP", {
           style: "currency",
           currency: "JPY",
           minimumFractionDigits: 0,
         }),
-    remainingAUD: isNaN(remainingAUD)
-      ? "N/A"
-      : remainingAUD.toLocaleString("en-AU", {
+    remainingAUD: remainingAUD.toLocaleString("en-AU", {
           style: "currency",
           currency: "AUD",
           minimumFractionDigits: 2,
         }),
-    weeklyTargetJPY: isNaN(weeklyTargetJPY)
-      ? "N/A"
-      : weeklyTargetJPY.toLocaleString("ja-JP", {
+    weeklyTargetJPY: weeklyTargetJPY.toLocaleString("ja-JP", {
           style: "currency",
           currency: "JPY",
           minimumFractionDigits: 0,
         }),
-    weeklyTargetAUD: isNaN(weeklyTargetAUD)
-      ? "N/A"
-      : weeklyTargetAUD.toLocaleString("en-AU", {
+    weeklyTargetAUD: weeklyTargetAUD.toLocaleString("en-AU", {
           style: "currency",
           currency: "AUD",
           minimumFractionDigits: 2,
         }),
-    remainingWeeks: isNaN(remainingWeeks) ? "N/A" : remainingWeeks,
+    remainingWeeks : remainingWeeks,
   };
 };
