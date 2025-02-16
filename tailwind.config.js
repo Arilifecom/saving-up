4d42b4d1a2ed18db8d9f571601b27f643215ba59
Ari <arilife.com@gmail.com>
2025-02-16 17:28:23 +0900

    🐛 Fixing colors extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,24 +3,24 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    ontFamily: {
+    fontFamily: {
       zen: ['"Zen Maru Gothic"', "sans-serif"],
     },
     screens: {
       md: { min: "768px" },
     },
-    colors: {
-      bg_pink: "#FFEAE3",
-      headline: "#33272a",
-      textColor: "#594a4e",
-      blue: "#121481",
-      primary: "#ffc6c7",
-      light: "#ffffff",
-    },
     extend: {
       backgroundImage: {
         "wave-shape-xs": "url('/img/bg-wave-shape-xs.svg')",
         "wave-shape-xl": "url('/img/bg-wave-shape-xl.svg')",
+      },
+      colors: {
+        bg_pink: "#FFEAE3",
+        headline: "#33272a",
+        textColor: "#594a4e",
+        blue: "#121481",
+        primary: "#ffc6c7",
+        light: "#ffffff",
       },
     },
   },
