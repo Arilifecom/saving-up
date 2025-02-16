0ef1a73b3a33a158c81242b9a07d1e565632fddf
Ari <arilife.com@gmail.com>
2025-02-16 17:31:58 +0900

    ⚡️ Fixng indents

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,8 +6,7 @@ import ResultArea from "../components/ResultArea";
 import CustomAlert from "../components/CustomAlert";
 import { calculateResults } from "../utils/calculateResults";
 
-
-export default function Home({exchangeRate}) {
+export default function Home({ exchangeRate }) {
   //状態管理｜入力データ
   const [inputData, setInputData] = useState({
     targetJPY: "",
@@ -27,24 +26,24 @@ export default function Home({exchangeRate}) {
 
   //状態を更新｜handleInputChange関数｜propsでinpuAreaへ｜関数を通じてinpuAreaからデータ取得し、setInputDataで状態を更新（prevData）は前の状態を引きづいてname：に取得したvalueを渡す
   const handleInputChange = (name, value, cursorPos) => {
-     // targetDate の場合は処理をスキップ
-     if ( name === "targetDate") {
+    // targetDate の場合は処理をスキップ
+    if (name === "targetDate") {
       setInputData((prevData) => ({ ...prevData, [name]: value }));
       return;
-     };
+    }
 
     // 取得したデータのカンマを取り除いて、数字のみかどうかをチェック
-    const rawValue = value.replace(/,/g, '');
+    const rawValue = value.replace(/,/g, "");
 
     // 数字以外の入力を防止
     if (!/^[0-9]*$/.test(rawValue)) {
       setAlertMessage("半角数字のみ入力できます");
       return;
     }
-    
-      setAlertMessage(""); // エラーメッセージをリセット
+
+    setAlertMessage(""); // エラーメッセージをリセット
     // カンマを追加する関数
-    const formattedValue = rawValue.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    const formattedValue = rawValue.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     setInputData((prevData) => ({ ...prevData, [name]: formattedValue }));
 
     // カーソル位置を補正（カンマが増えた分だけ調整）
@@ -52,75 +51,71 @@ export default function Home({exchangeRate}) {
       const inputElement = document.querySelector(`input[name=${name}]`);
 
       if (inputElement) {
-        inputElement.selectionStart = cursorPos + (formattedValue.length - rawValue.length);
+        inputElement.selectionStart =
+          cursorPos + (formattedValue.length - rawValue.length);
         inputElement.selectionEnd = inputElement.selectionStart;
       }
     }, 0);
-
   };
-   
-    // 状態を更新｜handleSubmit関数｜エラーチェック｜ユーザー入力データをcalculateResultsコンポーネントへ渡す｜計算結果をsetResultsに渡し状態更新｜setShowResultsにtrueを渡す
-    const handleSubmit = () => {
-      const { targetJPY, currentValueJPY, currentValueAUD, targetDate } =
-        inputData;
-        
-      // 入力フィールドにデータがない時
-      if (
-        !targetJPY ||
-        !currentValueJPY ||
-        !currentValueAUD ||
-        !targetDate
-      ) {
-        setAlertMessage("入力されていない箇所があります。");
-        setShowResults(false);
-        return;
-      }
 
-      // 計算コンポーネントにユーザー入力データとレートを渡す
-      const calculatedResults = calculateResults(inputData, exchangeRate);
-      setResults(calculatedResults);
-      setShowResults(true);
-    };
+  // 状態を更新｜handleSubmit関数｜エラーチェック｜ユーザー入力データをcalculateResultsコンポーネントへ渡す｜計算結果をsetResultsに渡し状態更新｜setShowResultsにtrueを渡す
+  const handleSubmit = () => {
+    const { targetJPY, currentValueJPY, currentValueAUD, targetDate } =
+      inputData;
 
-    const closeAlert = () => {
-      setAlertMessage("");
-    };
-
-    //期日と目標金額再設定ボタンの処理
-    //ボタンクリックでrefを取得してページ内ジャンプ
-  
-    const changeTarget = () => {
-      // 目標期日セクションにスクロール
+    // 入力フィールドにデータがない時
+    if (!targetJPY || !currentValueJPY || !currentValueAUD || !targetDate) {
+      setAlertMessage("入力されていない箇所があります。");
       setShowResults(false);
-    };
-  
-  
+      return;
+    }
+
+    // 計算コンポーネントにユーザー入力データとレートを渡す
+    const calculatedResults = calculateResults(inputData, exchangeRate);
+    setResults(calculatedResults);
+    setShowResults(true);
+  };
+
+  const closeAlert = () => {
+    setAlertMessage("");
+  };
+
+  //期日と目標金額再設定ボタン後に非表示
+  const changeTarget = () => {
+    setShowResults(false);
+  };
+
   return (
     <>
       <div className="flex justify-end mb-4 md:max-w-xl px-4 mx-auto">
         <TodaysDate />
         <div>
-      <h2>現在のレート</h2>
-      <p className="border mr-2 rounded py-2 px-3.5">{exchangeRate ? exchangeRate : 'データの取得に失敗しました'}</p>
-    </div>
+          <h2>現在のレート</h2>
+          <p className="border border-headline mr-2 rounded py-2 px-3.5">
+            {exchangeRate ? exchangeRate : "データの取得に失敗しました"}
+          </p>
+        </div>
       </div>
-      <InputArea
-        inputData={inputData}
-        handleInputChange={handleInputChange}
-        exchangeRate={exchangeRate}
-      />
-      {alertMessage && (
-        <CustomAlert message={alertMessage} onClose={closeAlert} />
-      )}
-      <SubmitBtn onSubmit={handleSubmit} />
-      {showResults && <ResultArea results={results} changeTarget={changeTarget} />}
-      {alertMessage && (
-        <CustomAlert message={alertMessage} onClose={closeAlert} />
-      )}
+      <div className="pb-3">
+        <InputArea
+          inputData={inputData}
+          handleInputChange={handleInputChange}
+          exchangeRate={exchangeRate}
+        />
+        {alertMessage && (
+          <CustomAlert message={alertMessage} onClose={closeAlert} />
+        )}
+        <SubmitBtn onSubmit={handleSubmit} />
+        {showResults && (
+          <ResultArea results={results} changeTarget={changeTarget} />
+        )}
+        {alertMessage && (
+          <CustomAlert message={alertMessage} onClose={closeAlert} />
+        )}
+      </div>
     </>
   );
 }
-
 
 export async function getStaticProps() {
   const apiKey = process.env.NEXT_PUBLIC_EXCHANGE_RATE_API_KEY;
@@ -132,22 +127,21 @@ export async function getStaticProps() {
       throw new Error(`Failed to fetch data: ${res.status} ${res.statusText}`);
     }
     const data = await res.json();
-    const exchangeRate = parseFloat(data.conversion_rates.JPY).toFixed(2);    
+    const exchangeRate = parseFloat(data.conversion_rates.JPY).toFixed(2);
 
-    return{
-      props:{
+    return {
+      props: {
         exchangeRate,
       },
       revalidate: 86400,
     };
-    
-  }catch (error){
-    console.error('Error fetching exchange rate:', error.message);
+  } catch (error) {
+    console.error("Error fetching exchange rate:", error.message);
 
-  return{
-    props:{
-      exchangeRate: 'Error',
-    },
-  };
-}
+    return {
+      props: {
+        exchangeRate: "Error",
+      },
+    };
+  }
 }
