53e0a6e36c667081a3576c9ab1dfd7e53dd456eb
Ari <arilife.com@gmail.com>
2025-02-17 11:40:58 +0900

    🐛 Fixed states name(respons to res)

--- src/pages/api/exchangeRate.js
+++ src/pages/api/exchangeRate.js
@@ -2,11 +2,11 @@
 export default async function getExchangeRate(req, res) {
   const url = `https://v6.exchangerate-api.com/v6/${process.env.EXCHANGE_RATE_API_KEY}/latest/AUD`;
 
-  const res = await fetch(url);
-  if (!res.ok) {
+  const respons = await fetch(url);
+  if (!respons.ok) {
     return res.status(500).json({ error: "Failed to fetch exchange rates" });
   }
 
-  const data = await res.json();
+  const data = await respons.json();
   res.status(200).json(data);
 }
