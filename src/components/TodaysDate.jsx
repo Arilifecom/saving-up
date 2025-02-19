a35add4b0b96e81d8eed29e41d78899d9dcc61e0
Ari <arilife.com@gmail.com>
2025-02-19 14:24:19 +0900

    🐛 Fixed Hydration error for TodaysDate

--- src/components/TodaysDate.jsx
+++ src/components/TodaysDate.jsx
@@ -1,15 +1,18 @@
+import { useEffect, useState } from "react";
+
 const TodaysDate = () => {
-  const today = new Date();
-  const year = today.getFullYear();
-  const month = String(today.getMonth() + 1).padStart(2, "0");
-  const day = String(today.getDate()).padStart(2, "0");
-  const formattedDate = `${year}/${month}/${day}`;
+  const [formattedDate, setFormattedDate] = useState("");
+
+  useEffect(() => {
+    const today = new Date().toLocaleDateString();
+    setFormattedDate(today);
+  }, []);
 
   return (
     <div>
       <label>今日の日付</label>
       <p className="border border-headline mr-2 rounded py-2 px-4">
-        {formattedDate}
+        {formattedDate || "Loading..."}
       </p>
     </div>
   );
